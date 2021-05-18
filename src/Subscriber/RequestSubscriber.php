e5b3c00a88749d075645d66fc08a2addca23f656
rune.laenen <rune.laenen@intracto.com>
2021-05-18 16:38:25 +0200

    Allow redirects on redirects

--- src/Subscriber/RequestSubscriber.php
+++ src/Subscriber/RequestSubscriber.php
@@ -7,9 +7,12 @@ use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
+use Shopware\Core\Framework\Event\BeforeSendRedirectResponseEvent;
 use Shopware\Core\Framework\Event\BeforeSendResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class RequestSubscriber implements EventSubscriberInterface
 {
@@ -29,55 +32,75 @@ class RequestSubscriber implements EventSubscriberInterface
     {
         return [
             BeforeSendResponseEvent::class => 'redirectBeforeSendResponse',
+            BeforeSendRedirectResponseEvent::class => 'redirectBeforeRedirectResponse'
         ];
     }
 
     public function redirectBeforeSendResponse(BeforeSendResponseEvent $event): void
     {
-        $requestBase = $event->getRequest()->getPathInfo();
-        if (\strpos($requestBase, '/admin') === 0) {
-            return;
-        }
-        if (\strpos($requestBase, '/api') === 0) {
-            return;
-        }
-        if (\strpos($requestBase, '/widgets') === 0) {
-            return;
-        }
-        if (\strpos($requestBase, '/store-api') === 0) {
+        $response = $this->handleRequest($event->getRequest());
+        if (!$response) {
             return;
         }
 
-        $requestUri = (string) $event->getRequest()->get('resolved-uri');
+        $event->setResponse($response);
+    }
+
+    public function redirectBeforeRedirectResponse(BeforeSendRedirectResponseEvent $event): void
+    {
+        $requestUri = trim($event->getRequest()->getPathInfo(), '/');
+        $response = $this->handleRequest($event->getRequest(), $requestUri);
+
+        if (!$response) {
+            return;
+        }
+
+        $event->setResponse($response);
+    }
+
+    private function handleRequest(Request $request, ?string $requestUri = null): ?Response
+    {
+        $requestBase = $request->getPathInfo();
+        if (\strpos($requestBase, '/admin') === 0) {
+            return null;
+        }
+        if (\strpos($requestBase, '/api') === 0) {
+            return null;
+        }
+        if (\strpos($requestBase, '/widgets') === 0) {
+            return null;
+        }
+        if (\strpos($requestBase, '/store-api') === 0) {
+            return null;
+        }
+
+        $requestUri = $requestUri ?? (string) $request->get('resolved-uri');
 
         $context = Context::createDefaultContext();
         $redirects = $this->seoUrlRepository->search((new Criteria())
             ->addFilter(new EqualsAnyFilter('pathInfo', [$requestUri])), $context);
 
-        $storefrontUri = $event->getRequest()->get('sw-storefront-url');
-        $requestBaseUrl = $event->getRequest()->getBaseUrl();
+        $storefrontUri = $request->get('sw-storefront-url');
+        $requestBaseUrl = $request->getBaseUrl();
         $search = [];
 
         // if found overwrite search term with the seo route
         if ($redirects->count() !== 0) {
             foreach ($redirects as $redirect) {
-                $requestBase = $redirect->getSeoPathInfo();
+                $uri = $redirect->getSeoPathInfo();
                 // Search for Redirect
                 $search[] = [
-                    $requestBaseUrl . '/' . $requestBase, // relative url with shopware 6 in sub folder: /public/Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53
-                    $requestBaseUrl . $requestBase, // relative url with shopware 6 in sub folder url is not shopware seo url: /public/test
-                    $storefrontUri . $requestBase, // absolute url with shopware 6 in sub folder, full url with host: http://shopware-platform.local/public/test1
-                    $storefrontUri . '/' . $requestBase, // absolute url with shopware 6 in sub folder, full url with host and slash at the end: http://shopware-platform.local/public/Freizeit-Elektro/Telefone/
-                    $requestBase, // relative url domain configured in public folder: /Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53 or /test4
-                    '/' . $requestBase, // absolute url domain configured in public folder: http://shopware-platform.local/Shoes-Baby/
-                    \substr($requestBase, 1), // e.g. "test"
+                    $requestBaseUrl . '/' . $uri, // relative url with shopware 6 in sub folder: /public/Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53
+                    $requestBaseUrl . $uri, // relative url with shopware 6 in sub folder url is not shopware seo url: /public/test
+                    $storefrontUri . $uri, // absolute url with shopware 6 in sub folder, full url with host: http://shopware-platform.local/public/test1
+                    $storefrontUri . '/' . $uri, // absolute url with shopware 6 in sub folder, full url with host and slash at the end: http://shopware-platform.local/public/Freizeit-Elektro/Telefone/
+                    $uri, // relative url domain configured in public folder: /Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53 or /test4
+                    '/' . $uri, // absolute url domain configured in public folder: http://shopware-platform.local/Shoes-Baby/
+                    \substr($uri, 1), // e.g. "test"
                 ];
             }
-        }
-        if (!empty($search)) {
-            $search = array_merge(...$search);
         } else {
-            $search = [
+            $search[] = [
                 $requestBaseUrl . '/' . $requestUri, // relative url with shopware 6 in sub folder: /public/Ergonomic-Concrete-Cough-Machine/48314803f1244f609a2ce907bfb48f53
                 $requestBaseUrl . $requestUri, // relative url with shopware 6 in sub folder url is not shopware seo url: /public/test
                 $storefrontUri . $requestUri, // absolute url with shopware 6 in sub folder, full url with host: http://shopware-platform.local/public/test1
@@ -88,6 +111,10 @@ class RequestSubscriber implements EventSubscriberInterface
             ];
         }
 
+        $search = array_merge(...$search);
+
+        $search = array_unique($search);
+
         $criteria = (new Criteria())
             ->addFilter(new EqualsAnyFilter('source', $search))
             ->setLimit(1);
@@ -96,7 +123,7 @@ class RequestSubscriber implements EventSubscriberInterface
         $redirect = $this->redirectRepository->search($criteria, $context)->first();
 
         if (!$redirect) {
-            return;
+            return null;
         }
 
         $targetURL = $redirect->getTarget();
@@ -111,6 +138,6 @@ class RequestSubscriber implements EventSubscriberInterface
             }
         }
 
-        $event->setResponse(new RedirectResponse($targetURL, $redirect->getHttpCode()));
+        return new RedirectResponse($targetURL, $redirect->getHttpCode());
     }
 }
